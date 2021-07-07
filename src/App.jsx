e2272cfd581319e1c7aa898b1997330fb61ed458
clecardona <clecardona@gmail.com>
2021-07-07 15:44:07 +0200

    array of objects, persistence in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {RecoilRoot} from "recoil";
+
 import logo from "./assets/images/logo.png";
 import "./styles/App.scss";
 import ShoppingList from "./components/ShoppingList";
@@ -6,15 +8,15 @@ import ShoppingList from "./components/ShoppingList";
 export default  function App() {
 
 
-
-
   return (
+    <RecoilRoot>
     <div className="App">
       
-      <header className="header">header<img src={logo} alt="logo" /></header>
+      {/* <header className="header">header<img src={logo} alt="logo" /></header> */}
       <main><ShoppingList/></main>
-      <footer>footer</footer>
+      {/* <footer>footer</footer> */}
     </div>
+    </RecoilRoot>
   );
 }
 
--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,5 +1,12 @@
-import { useState, useEffect,React } from "react";
-import Item from "./Item"
+import { useState, useEffect, React } from "react";
+import {
+  atom,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+
+import Item from "./Item";
 
 export default function ShoppingList() {
   var item0 = {
@@ -9,24 +16,62 @@ export default function ShoppingList() {
     price: 79,
     acquired: true,
   };
-// Put the object into storage
-localStorage.setItem('item0', JSON.stringify(item0));
-// Retrieve the object from storage
-var retrievedObject = JSON.parse(localStorage.getItem('item0'));
 
-  const [data, setData] = useState([retrievedObject]);
 
-  //console.log(data);
+
+ 
+  // CONSTANTS
+  const [text, setText] = useState("");
+  const [price, setPrice] = useState(0);
+  const [list,setList] = useState([item0]);
+  
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list])
+
+  var retrievedList = JSON.parse(localStorage.getItem("list")); 
+
+
+const addItemToList = (e) => {
+  e.preventDefault();
+
+const newItem = {
+  imageUrl:
+    "https://www.ikea.com/global/assets/navigation/images/mattresses-bm002.jpeg?imwidth=300",
+  name: text,
+  price: price,
+  acquired: false,
+};
+
+  const newList = [...list]
+  newList.push(newItem)
+
+  setList(newList)
+  e.target.reset()
+}
+
+  /* console.log("text",text)
+  console.log("price",price) */
+  console.log("list",list)
+  console.log("retrievedList",retrievedList)
+
 
   return (
     <section className="shopping_list">
       <h1>My Shopping-List</h1>
-      
+
       {/* <React.Fragment> */}
-      <Item data ={data}/>
-      <Item data ={data}/>
+      {/* <Item data ={data}/> */}
       {/* </React.Fragment> */}
 
+
+<form onSubmit={addItemToList} >
+
+  <input type="text" id="name" name="name" onChange={(e) => setText(e.target.value)} placeholder="enter a new item..."></input> 
+  <input type="text" id="price" name="price" onChange={(e) => setPrice(e.target.value)} placeholder="price"></input> 
+  
+  <input type="submit" value="Add item"></input>
+</form>
     </section>
   );
 }
