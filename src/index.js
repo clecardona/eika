9635f6541a5ad03f7ac1da55ffa0d75a11b91e52
clecardona <clecardona@gmail.com>
2021-07-19 10:38:32 +0200

    errorBoundary

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import ErrorBoundary from "./ErrorBoundary"
 
 import App from './App'
 //import * as serviceWorker from './serviceWorker'
@@ -10,7 +11,9 @@ import './index.css'
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
+      <ErrorBoundary>
       <App />
+      </ErrorBoundary>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
